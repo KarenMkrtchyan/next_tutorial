01f6eea454ec302cbdb7a9c999f3ba3202bc48e2
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-07-27 18:33:14 -0700

    rand err

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,6 @@
 import { Inter, Lusitana } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: "400" });
+export const lusitana = Lusitana({
+  subsets: ["latin"], // ✅ Add this line
+  weight: "400",
+});
